45e34e2b32299b1d61d71b0a5729189a64a61d22
flaviedaniela <flavienkenang@gmail.com>
2023-03-09 19:15:33 +0100

    modification

--- test-app/App.js
+++ test-app/App.js
@@ -16,21 +16,24 @@ export default function App() {
 }
 */
 
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import HomePage from './src/screens/HomePage';
 import FlatlistScreen1 from './src/screens/FlatlistScreen1';
 import FlatlistScreen2 from './src/screens/FlatlistScreen2';
 
 const Stack = createStackNavigator();
 
-const App=()=> {
+export default function App() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomePage} />
-      <Stack.Screen name="Notifications" component={FlatlistScreen1} />
-      <Stack.Screen name="Profile" component={FlatlistScreen2} />
-
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="FlatlistScreen1" component={FlatlistScreen1} />
+        <Stack.Screen name="FlatlistScreen2" component={FlatlistScreen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-export default App;
+}
